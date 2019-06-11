1520529553447877828ef164b2cba52d41291c31
Yosuke Shibayama <yosukeshibayama@gmail.com>
2019-06-11 22:12:05 +0900

    vuetify tab UI追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1></h1>
+  <div>
+    <v-tabs v-model="tab" color="" grow>
+      <v-tabs-slider color=""></v-tabs-slider>
+      <v-tab v-for="item in 3" :key="item">
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in 3"
+        :key="item"
+        :transition="false"
+        :reverse-transition="false"
+      >
+        <HelloWorld v-if="item === 1" />
+        <HelloWorld v-if="item === 2" />
+        <HelloWorld v-if="item === 3" />
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld";
+
 export default {
-  components: {}
+  components: { HelloWorld },
+  data() {
+    return {
+      tab: null
+    };
+  },
+  methods: {}
 };
 </script>
